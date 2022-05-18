ae0b61db46abe5008df523d32b60582f2447212f
Fathy <abdelrahmanfathe95@gmail.com>
2022-05-18 15:21:38 +0200

    add unit validation

---
+++ CSharp.Functional/Extensions/ErrorExtension.cs
@@ -0,0 +1,22 @@
+﻿using CSharp.Functional.Constructs;
+using CSharp.Functional.Errors;
+using System.Collections.Generic;
+using System.Linq;
+using static CSharp.Functional.Extensions.ValidationExtension;
+using static CSharp.Functional.Functional;
+using Unit = System.ValueTuple;
+
+namespace CSharp.Functional.Extensions
+{
+    public static class ErrorExtension
+    {
+        public static Validation<Unit> AsUnitValidation(this IEnumerable<Error> errs)
+        {
+            if (errs.Count() > 0)
+                return Invalid(errs);
+            else
+                return Unit();
+        }
+
+    }
+}
