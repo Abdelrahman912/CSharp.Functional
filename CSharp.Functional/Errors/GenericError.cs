d308e5a3aecc83590361ab470cd8a68eab1dfcd4
Fathy <abdelrahmanfathe95@gmail.com>
2022-06-30 15:10:32 +0200

    add generic error

---
+++ CSharp.Functional/Errors/GenericError.cs
@@ -0,0 +1,21 @@
+﻿namespace CSharp.Functional.Errors
+{
+    public class GenericError : Error
+    {
+        #region Properties
+
+        override public string Message { get; }
+
+        #endregion
+
+        #region Constructors
+
+        public GenericError(string message)
+        {
+            Message = message;
+        }
+
+        #endregion
+
+    }
+}
